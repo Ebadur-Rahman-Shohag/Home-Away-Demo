2d8fa88d5d74f9aa5f743c1c6101cdc4a33c97aa
Ebadur Rahman <ebadurshohag@gmail.com>
2024-11-28 02:31:24 +0600

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "img.clerk.com",
+            },
+            {
+                protocol: "https",
+                hostname: "ntdelfyttfcucnrxbcyl.supabase.co",
+            },
+        ],
+    },
+};
+
+export default nextConfig;
+
+("ntdelfyttfcucnrxbcyl.supabase.co");
